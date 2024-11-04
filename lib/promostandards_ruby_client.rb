103718a75fa6fd8208391aa9e9b585c1ba93cce8
Erich N Quintero <qbantek@gmail.com>
2024-11-04 13:40:40 -0500

    Fix invalid namespace
    Correct namespace is PromostandardsRubyClient not
    PromoStandardsRubyClient.

--- lib/promostandards_ruby_client.rb
+++ lib/promostandards_ruby_client.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# PromoStandardsRubyClient is an unofficial Ruby client for interacting with
+# PromostandardsRubyClient is an unofficial Ruby client for interacting with
 # PromoStandards API services, providing a flexible configuration interface
 # for accessing services such as Product Data, Media Content, and Pricing.
 #
 # This gem allows users to set up a client with options for service URL and
 # logging, making it easy to integrate PromoStandards functionality into
 # Ruby applications.
-module PromoStandardsRubyClient
+module PromostandardsRubyClient
   require_relative "promostandards_ruby_client/version"
   require_relative "promostandards_ruby_client/client"
   require_relative "promostandards_ruby_client/configuration"
